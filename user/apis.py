8d179b9ea54d85a059585d6dd77e1f5168cc52ae
Seamile <lanhuermao@gmail.com>
2020-09-23 11:10:54 +0800

    1. 添加了短信发送模块 2. 完成了发送验证码接口

--- user/apis.py
+++ user/apis.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from user.logics import send_vcode
+
+
+def fetch_vcode(request):
+    '''给用户发送验证码'''
+    phonenum = request.GET.get('phonenum')
+    if send_vcode(phonenum):
+        return JsonResponse({'code': 0, 'data': None})
+    else:
+        return JsonResponse({'code': 1000, 'data': None})
+
+
+def submit_vcode(request):
+    '''提交验证码，执行登录注册'''
+    return JsonResponse()
+
+
+def show_profile(request):
+    '''查看个人资料'''
+    return JsonResponse()
+
+
+def update_profile(request):
+    '''更新个人资料'''
+    return JsonResponse()
+
+
+def qn_token(request):
+    '''获取七牛云 Token'''
+    return JsonResponse()
+
+
+def qn_callback(request):
+    '''七牛云回调接口'''
+    return JsonResponse()
